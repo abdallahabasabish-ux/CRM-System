697f2fbc15a99b563965bc5f1bde63aff6b4f99a
Abdallah Abas <abdallahabasabish@gmail.com>
2026-08-15 01:54:09 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDSAf_nl-72K0VsyvCkNvLLn5qzHw-ZRMI",
   authDomain: "crm-system-2ee41.firebaseapp.com",
@@ -11,13 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "190840060045",
   appId: "1:190840060045:web:4b151ba2201b63f9754ac4"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-
 setPersistence(auth, browserLocalPersistence)
-  .catch((error) => console.warn("Persistence error:", error));
-
+.catch((error) => console.warn("Persistence error:", error));
 export { auth, db, storage, app };
