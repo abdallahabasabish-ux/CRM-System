0bed5bfdb17292eafe17649f762bede91018ac33
Abdallah Abas <abdallahabasabish@gmail.com>
2026-08-15 01:57:46 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDSAf_nl-72K0VsyvCkNvLLn5qzHw-ZRMI",
   authDomain: "crm-system-2ee41.firebaseapp.com",
   projectId: "crm-system-2ee41",
   storageBucket: "crm-system-2ee41.firebasestorage.app",
   messagingSenderId: "190840060045",
-  appId: "1:190840060045:web:4b151ba2201b63f9754ac4"
+  appId: "1:190840060045:web:e53b90fa8e1516ee754ac4"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
